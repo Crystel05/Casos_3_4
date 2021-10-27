553df63e75ed2d8a2b65e311631d7c2d45222972
Crystel Montero <crysvane05@estudiantec.cr>
2021-10-26 22:49:12 -0600

    Ventanas de subastas

---
+++ Subastas/src/Vista/Subastar.java
@@ -0,0 +1,19 @@
+package Vista;
+
+import Model.DragWindow;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.Pane;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Subastar implements Initializable, DragWindow {
+    @FXML
+    private Pane contenedor;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        this.onDraggedScene(contenedor);
+    }
+}
