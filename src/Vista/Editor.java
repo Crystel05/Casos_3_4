8a417a34996ba47dd0192ebe5b5d08a8c526970b
Crystel Montero <crysvane05@estudiantec.cr>
2021-10-28 23:21:48 -0600

    ventanas

---
+++ src/Vista/Editor.java
@@ -0,0 +1,66 @@
+package Vista;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.text.TextFlow;
+
+public class Editor {
+
+    @FXML
+    private TextFlow areaMostrar;
+
+    @FXML
+    private TextArea areaEscribir;
+
+    @FXML
+    public void crear(MouseEvent event){
+
+    }
+
+    @FXML
+    public void abir(MouseEvent event){
+
+    }
+
+    @FXML
+    public void guardar(MouseEvent event){
+
+    }
+
+    @FXML
+    public void guardarComo(MouseEvent event){
+
+    }
+
+    @FXML
+    public void resaltar(MouseEvent event){
+
+    }
+
+    @FXML
+    public void copiar(MouseEvent event){
+
+    }
+
+    @FXML
+    public void cortar(MouseEvent event){
+
+    }
+
+    @FXML
+    public void pegar(MouseEvent event){
+
+    }
+
+    @FXML
+    public void undo(MouseEvent event){
+
+    }
+
+    @FXML
+    public void redo(MouseEvent event){
+
+    }
+
+}
