4139a6d3ea9e60e07c80775e4814ef618d4e1960
Crystel Montero <crysvane05@estudiantec.cr>
2021-10-28 20:04:50 -0600

    Más ventanas

--- Subastas/src/Vista/Main.java
+++ Subastas/src/Vista/Main.java
@@ -21,7 +21,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("FXMLS/principal.fxml")));
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("FXMLS/Felicitacion.fxml")));
         primaryStage.setScene(new Scene(root));
         primaryStage.initStyle(StageStyle.TRANSPARENT);
         primaryStage.setResizable(false);
