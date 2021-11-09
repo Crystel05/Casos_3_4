7b60e0d3d500c46e524b3a6748535a65aa6a39ef
Fernando Alvarez <ferolsen@hotmail.com>
2021-11-08 23:25:29 -0600

    Nombres

--- Subastas/src/Vista/ServerMain.java
+++ Subastas/src/Vista/ServerMain.java
@@ -6,7 +6,14 @@ import ProjectNetwork.AuctionServerRequestHandler;
 
 import java.io.IOException;
 
-
+/**
+ * Caso 3: Editor de texto
+ * Fabrizio Ferreto 2019177147
+ * Ulises Rodríguez 2019380067
+ * Natalia Vargas   2019077180
+ * Fernando Álvarez 2019171657
+ * Crystel Montero  2019158736
+ * */
 
 public class ServerMain {
     public static void main(String[] args) {
